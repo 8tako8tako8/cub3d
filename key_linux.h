d91d53bc9b99eda393f2965777414fe218dbe8ca
森本晃輔 <8tako8tako8@MacBook-Pro.local>
2020-12-20 13:35:24 +0900

    linux Makefile

---
+++ key_linux.h
@@ -0,0 +1,13 @@
+#ifndef KEY_LINUX_H
+# define KEY_LINUX_H
+
+# define K_W
+# define K_S
+# define K_A
+# define K_D
+# define K_UP
+# define K_DOWN
+# define K_LEFT
+# define K_RIGHT
+
+#endif
--- key_macos.h
+++ key_macos.h
@@ -1,10 +1,6 @@
 #ifndef KEY_MACOS_H
 # define KEY_MACOS_H
 
-/*
-** Keyboard LETTERS
-*/
-
 # define K_A 0
 # define K_B 11
 # define K_C 8
@@ -31,20 +27,10 @@
 # define K_X 7
 # define K_Y 16
 # define K_Z 6
-
-/*
-** Keyboard ARROWS
-*/
-
 # define K_AR_L 123
 # define K_AR_R 124
 # define K_AR_U 126
 # define K_AR_D 125
-
-/*
-** Keyboard NUMPAD
-*/
-
 # define K_NP_DIV 75
 # define K_NP_MUL 67
 # define K_NP_MIN 78
@@ -58,19 +44,9 @@
 # define K_NP_7	89
 # define K_NP_8	91
 # define K_NP_9	92
-
-/*
-** Keyboard MISC
-*/
-
 # define K_SP 49
 # define K_ESC 53
 # define K_TAB 48
-
-/*
-** Mouse
-*/
-
 # define M_CLK_L 1
 # define M_CLK_R 3
 # define M_CLK_M 2
